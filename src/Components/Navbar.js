f5b020eab53991bfe573e82df5b8bcc3f547daf8
umi <ianlumanog.iml@gmail.com>
2023-10-29 15:42:02 +0800

    Updated: Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,24 @@
-import {Component} from 'react';
+import { Component } from 'react';
 import { MenuItems } from './MenuItems';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes, faBars } from '@fortawesome/free-solid-svg-icons';
 import './NavbarStyles.css';
 
 class Navbar extends Component{
+    state = { clicked: false};
+    handleClick = () => {
+        this.setState({clicked : !this.state.clicked});
+    }
     render(){
         return(
             <nav className="NavbarItems">
                 <h1 className="navbar-logo">Lerancy</h1>
 
-                <ul className="nav-menu">
+                <div className="menu-icons" onClick={this.handleClick}>
+                    <FontAwesomeIcon icon={this.state.clicked ? faTimes : faBars} className='ham'/>
+                </div>
+
+                <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
                     {MenuItems.map((item, index) =>{
                         return(
                           <li key = {index}>
