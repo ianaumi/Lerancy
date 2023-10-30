4af298a52f5d1e35dba440230ae96eae5b383047
umi <ianlumanog.iml@gmail.com>
2023-10-30 17:02:09 +0800

    Added: Hero Component

---
+++ src/Components/Hero.js
@@ -0,0 +1,18 @@
+import './HeroStyles.css';
+
+function Hero(){
+    return(
+        <>
+        <div className="hero">
+            <img alt="HeroImg" src='https://wallpapers.com/images/featured/reading-book-gqr32547jwegknzo.jpg'/>
+        </div>
+
+        <div className="hero-text">
+            <h1>Lerancy</h1>
+            <p>Explore the web with confidence</p>
+        </div>
+        </>
+    )
+}
+
+export default Hero;
