1eaf47d7f2b0587dca3c515288b5992be2d50d34
umi <ianlumanog.iml@gmail.com>
2023-11-24 02:38:01 +0800

    Removed: one card

--- src/Components/Resources.js
+++ src/Components/Resources.js
@@ -15,17 +15,26 @@ function Resources () {
                 heading={"Digital Literate Citizens: Think Critically, Click Wisely!"}
                 text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam in aliquet mi, ultricies faucibus justo. Nam aliquam auctor aliquet. Proin lacinia ante blandit tincidunt vestibulum. Vivamus lobortis dolor justo, sed lacinia enim commodo in. Sed eleifend, mauris eu mollis tincidunt, quam libero vulputate lorem, sed lacinia felis libero vel arcu. Mauris porttitor tincidunt dignissim. Nam ante metus, dapibus eu molestie ut, viverra commodo augue. Vestibulumsque cursus convallis diam, sit amet dictum felis efficitur eget. Se"}
                 />
+
                 <Card
                 image={card2}
                 heading={"Five Laws of Digital Literacy"}
                 text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam in aliquet mi, ultricies faucibus justo. Nam aliquam auctor aliquet. Proin lacinia ante blandit tincidunt vestibulum. Vivamus lobortis dolor justo, sed lacinia enim commodo in. Sed eleifend, mauris eu mollis tincidunt, quam libero vulputate lorem, sed lacinia felis libero vel arcu. Mauris porttitor tincidunt dignissim. Nam ante metus, dapibus eu molestie ut, viverra commodo augue. Vestibulumsque cursus convallis diam, sit amet dictum felis efficitur eget. Se"}
                 />
+
+                <Card
+                image={card3}
+                heading={"Digital Competencies and Skills"}
+                text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam in aliquet mi, ultricies faucibus justo. Nam aliquam auctor aliquet. Proin lacinia ante blandit tincidunt vestibulum. Vivamus lobortis dolor justo, sed lacinia enim commodo in. Sed eleifend, mauris eu mollis tincidunt, quam libero vulputate lorem, sed lacinia felis libero vel arcu. Mauris porttitor tincidunt dignissim. Nam ante metus, dapibus eu molestie ut, viverra commodo augue. Vestibulumsque cursus convallis diam, sit amet dictum felis efficitur eget. Se"}
+                />
+
                 <Card
                 image={card3}
                 heading={"Digital Competencies and Skills"}
                 text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam in aliquet mi, ultricies faucibus justo. Nam aliquam auctor aliquet. Proin lacinia ante blandit tincidunt vestibulum. Vivamus lobortis dolor justo, sed lacinia enim commodo in. Sed eleifend, mauris eu mollis tincidunt, quam libero vulputate lorem, sed lacinia felis libero vel arcu. Mauris porttitor tincidunt dignissim. Nam ante metus, dapibus eu molestie ut, viverra commodo augue. Vestibulumsque cursus convallis diam, sit amet dictum felis efficitur eget. Se"}
                 />
             </div>
+            <hr></hr>
         </div>
     )
 }
